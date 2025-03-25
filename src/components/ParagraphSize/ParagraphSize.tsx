f2bbc4909bcf3e691ea0b588152dc014e379c933
DiegoSimal <simaldiego6@gmail.com>
2025-03-25 13:49:18 +0100

    Dont view text problem resolve

--- src/components/ParagraphSize/ParagraphSize.tsx
+++ src/components/ParagraphSize/ParagraphSize.tsx
@@ -1,3 +1,3 @@
 export default function ParagraphSize({paragraph}: {paragraph: string}){
-    return <p className="pl-10 text-center">{paragraph}</p>
+    return <p className="pl-10 text-center text-blue-600">{paragraph}</p>
 }
--- src/components/TitleSize/TitleSize.tsx
+++ src/components/TitleSize/TitleSize.tsx
@@ -1,3 +1,3 @@
 export default function TitleSize({title}: {title: string}){
-    return <h2 className="text-blue-500 text-center">{title}</h2>
+    return <h2 className="text-blue-800 text-center">{title}</h2>
 } 
